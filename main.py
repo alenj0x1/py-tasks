163da1721568365b6aeef35d40f4e7a13fbee805
alenj0x1 <140274341+alenj0x1@users.noreply.github.com>
2025-08-10 14:56:32 -0500

    feat: implemented update and mark commands - Added TaskStatus enum. - Implemented get_all method in TaskManager class. - Included created_at and updated_at properties in Task class. - Improved list command with --status argument for filter proposes. - Fixed for add and update commands that can receive n arguments

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
-import argparse
+from enum import Enum
+from datetime import datetime
 
 
-# Tasks core
+class TaskStatus(Enum):
+    TODO = 1
+    IN_PROGRESS = 2
+    COMPLETED = 3
+
+
 class Task:
     id: int
     descriptor: str
-    status: int
+    status: TaskStatus
+    created_at: datetime
+    updated_at: datetime
 
-    def __init__(self, id: int, descriptor: str, status: int = 1):
+    def __init__(
+        self,
+        id: int,
+        descriptor: str,
+        status: int = 1,
+        created_at: str = datetime.now().isoformat(),
+        updated_at: str = datetime.now().isoformat(),
+    ):
         self.id = id
         self.descriptor = descriptor
         self.status = status
+        self.created_at = created_at
+        self.updated_at = updated_at
 
     def to_dict(self):
-        return {"id": self.id, "descriptor": self.descriptor, "status": self.status}
+        return {
+            "id": self.id,
+            "descriptor": self.descriptor,
+            "status": self.status,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        }
 
 
 import json
@@ -34,20 +57,37 @@ class TaskManager:
         task.id = len(self.tasks) + 1
         self.tasks.append(task)
 
-    # get a task
+    # get a task by id
     def get(self, id: int):
         for task in self.tasks:
             if task.id == id:
                 return task
 
+    def get_all(self, status: str | None = None):
+        if status is None:
+            return self.tasks
+
+        tasks: list[Task] = []
+        for task in self.tasks:
+            if task.status == TaskStatus[status].value:
+                tasks.append(task)
+
+        return tasks
+
     # update descriptor of a task
-    def update(self, id: int, descriptor: str):
+    def update(self, id: int, descriptor: str | None = None, status: str | None = None):
         task = self.get(id)
 
         if task is None:
             raise Exception(f"task with id {id} not found")
 
-        task.descriptor = descriptor
+        if descriptor is not None:
+            task.descriptor = descriptor
+
+        if status is not None:
+            task.status = TaskStatus[status].value
+
+        task.updated_at = datetime.now().isoformat()
 
     # remove a task
     def remove(self, id: int):
@@ -77,22 +117,56 @@ class TaskManager:
             )
 
 
+import argparse
+
+
 class TaskInterpreter:
     def __init__(self):
         # User input interpreter
         self.parser = argparse.ArgumentParser(description="Task Manager v1")
         subparsers = self.parser.add_subparsers(dest="command", required=True)
 
-        # User input interpreter - Add
+        # add
         add_parser = subparsers.add_parser("add", help="Add a new task")
         add_parser.add_argument(
-            "descriptor", help="Task descriptor. Example: Complete project integration"
+            "descriptor",
+            help="Task descriptor. Example: Complete project integration",
+            nargs=argparse.REMAINDER,
         )
 
-        # User input interpreter - List
-        add_parser = subparsers.add_parser("list", help="List all tasks")
+        # list
+        list_parser = subparsers.add_parser("list", help="List all tasks")
+        list_parser.add_argument(
+            "--status",
+            type=str,
+            help="Task status",
+            choices=[status.name for status in TaskStatus],
+        )
 
-        # User input interpreter - Remove
+        # mark
+        mark_parser = subparsers.add_parser(
+            "mark", help=f"Mark a task as: {[status.name for status in TaskStatus]}"
+        )
+        mark_parser.add_argument(
+            "id",
+            type=int,
+            help="Task id",
+        )
+        mark_parser.add_argument(
+            "status",
+            type=str,
+            help="Task status",
+            choices=[status.name for status in TaskStatus],
+        )
+
+        # update
+        update_parser = subparsers.add_parser("update", help="Update a task")
+        update_parser.add_argument("id", type=int, help="Task id")
+        update_parser.add_argument(
+            "descriptor", type=str, help="Task descriptor", nargs=argparse.REMAINDER
+        )
+
+        # remove
         remove_parser = subparsers.add_parser("remove", help="Remove a new task")
         remove_parser.add_argument("id", type=int, help="Task id")
 
@@ -114,7 +188,7 @@ class TaskUI:
         print(Fore.RED + message + Fore.RESET)
 
     def normal(self, message: str):
-        print(Fore.LIGHTYELLOW_EX + message + Fore.RESET)
+        print(Fore.LIGHTWHITE_EX + message + Fore.RESET)
 
 
 # UI manager
@@ -126,30 +200,56 @@ class TaskExecutor:
         self.interpreter = interpreter
         self.ui = ui
 
-        # UI manager - Add
-        if interpreter.args().command == "add":
-            new_task = Task(1, interpreter.args().descriptor)
+        args = interpreter.args()
+
+        # add
+        if args.command == "add":
+            new_task = Task(1, descriptor=str.join(" ", args.descriptor))
 
             manager.add(new_task)
             manager.save_to_json()
             ui.success(f"Task {new_task.id} added")
 
-        # UI manager - List
-        if interpreter.args().command == "list":
+        # list
+        if args.command == "list":
             ui.normal(
                 tabulate(
-                    [[task.id, task.descriptor, task.status] for task in manager.tasks],
-                    headers=["ID", "Descriptor", "Status"],
-                    tablefmt="grid",
+                    [
+                        [
+                            task.id,
+                            task.descriptor,
+                            TaskStatus(task.status).name,
+                            task.created_at,
+                            task.updated_at,
+                        ]
+                        for task in manager.get_all(args.status)
+                    ],
+                    headers=["ID", "Descriptor", "Status", "Created at", "Updated at"],
                 )
             )
 
-        # UI manager - Remove
-        if interpreter.args().command == "remove":
+        if args.command == "mark":
             try:
-                manager.remove(id=int(interpreter.args().id))
+                manager.update(id=args.id, status=args.status)
                 manager.save_to_json()
-                ui.success(f"Task with ID {interpreter.args().id} removed")
+                ui.success(f"Task with ID {args.id} now is marked as: {args.status}")
+            except Exception as e:
+                ui.error(str(e))
+
+        if args.command == "update":
+            try:
+                manager.update(id=args.id, descriptor=str.join(" ", args.descriptor))
+                manager.save_to_json()
+                ui.success(f"Task with ID {args.id} updated")
+            except Exception as e:
+                ui.error(str(e))
+
+        # remove
+        if args.command == "remove":
+            try:
+                manager.remove(id=int(args.id))
+                manager.save_to_json()
+                ui.success(f"Task with ID {args.id} removed")
             except Exception as e:
                 ui.error(str(e))
 
